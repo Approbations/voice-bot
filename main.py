ec6f19803ee6ff378b31ffe130a40d120e06e8eb
Approbations <120583081+Approbations@users.noreply.github.com>
2023-04-15 16:48:42 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+import dp as dp
 import telebot
 import uuid  # случайное имя файла
 import os
 import speech_recognition as sr
+import requests
+import subprocess
+import datetime
+from django.conf import settings
+
 
 from telebot import types
 
+
 language = 'ru_RU'  # бот работает только на русском
-TOKEN = '6287234764'  # от тг бота @BotFather
+TOKEN = settings.MY_API_KEY  # от тг бота @BotFather
 bot = telebot.TeleBot(TOKEN)
 r = sr.Recognizer()
 
@@ -18,12 +25,17 @@ def recognise(filename):
             text = r.recognize_google(audio_text, language=language)  # открываем аудиофайл, передаем в
             # r.recognize_google()
             return text
-        except:
+        except Exception:
             print('Прости, не расслышал')
             return 'Прости, не расслышал'
 
 
-@bot.message_handler(content_types=['start'])
+@bot.message_handler(commands=["start"])
+async def cmd_start(message: types.Message):
+    await message.reply(
+        "Привет! Это Бот для озвучки персонажей! И не забываем про пасхалки =). Для продолжения выберите 'help'")
+
+
 def voice_processing(message):
     filename = str(uuid.uuid4())
     file_name_full = "./voice/" + filename + ".ogg"
@@ -38,17 +50,57 @@ def voice_processing(message):
     os.remove(file_name_full)
     os.remove(file_name_full_converted)
 
+tts = TTS()
+@dp.message_handler(content_types=["help"])
+async def cmd_text(message: types.Message):
+    await message.reply("Текст получен")
+
+    out_filename = tts.text_to_ogg(message.text)
+
+    path = Path("", out_filename)                      # отправка гс
+    voice = InputFile(path)
+    await bot.send_voice(message.from_user.id, voice,
+                         caption="Ответ от бота")
+
+    os.remove(out_filename)                            # Удаление временного файла
 
 @bot.message_handler(commands=['start'])
 def start(message):
-  markup = types.ReplyKeyboardMarkup()
-  buttonA = types.KeyboardButton('A')         # пример изменения клавиатуры (с букв на выбор варианта)
-  buttonB = types.KeyboardButton('B')
-  buttonC = types.KeyboardButton('C')
+    markup = types.ReplyKeyboardMarkup()
+    button_Pumba = types.KeyboardButton('Пумба') # из "Король лев"
+    button_Osel = types.KeyboardButton('Осёл') # из шрека
+    button_Tayler = types.KeyboardButton('Тайлер Дердан') # из "Бойцовский клуб"
+    button_Vurhiz = types.KeyboardButton('Джейсон Вурхиз') # из "пятница 13"
+    button_Android = types.KeyboardButton('Андроид') # из игры "Detroit: Become Human"
+    button_Gomer = types.KeyboardButton('Гомер Симпсон')
 
-  markup.row(buttonA, buttonB)
-  markup.row(buttonC)
-  bot.send_message(message.chat.id, 'Скоро голос персонажа будет готов!', reply_markup=markup)
+    markup.row(button_Pumba)
+    markup.row(button_Osel)
+    markup.row(button_Tayler)
+    markup.row(button_Vurhiz)
+    markup.row(button_Vurhiz)
+    markup.row(button_Gomer)
+    markup.row(button_Android)
+    bot.send_message(message.chat.id, 'Скоро голос персонажа будет готов!', reply_markup=markup)
 
+if message.text.lower() == "Забудь заботы и держи трубой хвост":
+    await bot.send_message(message.chat.id, "Вот и весь секрет живи сто лет акуна матата!")
+if message.text.lower() == "Ты ходячая котострофа":
+    await bot.send_message(message.chat.id, "а ты ослостолоп")
+if message.text.lower() == "первое правило клуба":
+    await bot.send_message(message.chat.id, "не упоминать о бойцовском клубе")
+if message.text.lower() == "быть мертвецом не проблема":
+    await bot.send_message(message.chat.id, "быть забытым вот это гадко")
+if message.text.lower() ==  "Двадцать восемь ударов ножом":
+    await bot.send_message(message.chat.id, "Ты действовал наверняка да")
+if message.text.lower() == "Что вы выберете пиво или спасение души":
+    await bot.send_message(message.chat.id, "А какое пиво")
+''' Пасхалки:
+Пумба - "Забудь заботы И держи трубой хвост!" -> "Вот и весь секрет Живи сто лет Акуна матата!"
+Осёл - "Ты ходячая котострофа" —> "а ты ослостолоп"
+Тайлер - "первое правило клуба" -> "не упоминать о бойцовском клубе"
+Джейсон - "быть мертвецом не проблема" -> "быть забытым вот это гадко"
+Андроид - "28 ударов ножом" -> "Ты действовал наверняка, да" (из игры "Detroit: Become Human")
+Гомер - "Что вы выберете пиво или спасение души" -> "А какое пиво'''
 
 bot.polling()
